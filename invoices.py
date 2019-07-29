329be0e1150b3f8c24ac62b22c5997d91f91cdc6
Carlos Morán <carlosmoran.97cr@gmail.com>
2019-07-29 15:57:22 -0600

    tomando en cuenta la fecha de la factura

--- invoices.py
+++ invoices.py
@@ -22,18 +22,19 @@ for i in range(initial_row, max_row + 1):
 
     for j in range(initial_column, max_column + 1):
 
-        cell_obj = sheet.cell(row=i, column=j)
-
         # print(cell_obj.value, end=' | ')
         # escribir la sentencia sql
         if j == max_column:
+            cell_obj = sheet.cell(row=i, column=j-1)
             if cell_obj.value != None:
-                cell_obj_fisico = sheet.cell(row=i, column=j-1)
-                cell_obj_fecha = sheet.cell(row=i, column=j-1)
+				# correlativo de la factura en físico
+                cell_obj_fisico = sheet.cell(row=i, column=j-2)
+				# fecha de la factura
+                cell_obj_fecha = sheet.cell(row=i, column=j)
                 sql = """
                         BEGIN TRANSACTION;
-                        UPDATE account_invoice SET number=\'{}\' WHERE number=\'{}\';
+                        UPDATE account_invoice SET number='{}' WHERE number='{}' AND date_invoice='{}';
                         COMMIT TRANSACTION;
                       """.format(
-                    cell_obj.value, cell_obj_fisico.value)
+                    cell_obj.value, cell_obj_fisico.value, cell_obj_fecha.value)
                 print(sql)
